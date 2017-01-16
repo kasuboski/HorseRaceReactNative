a42c8523f8719356ec2af6a1dc8cc01e75b55398
Josh Kasuboski <kasuboski@wisc.edu>
2017-01-16 17:34:28 -0600

    cards move down by themselves

--- components/App.js
+++ components/App.js
@@ -10,7 +10,7 @@ import HorseCards from './HorseCards.js';
 
 import { generateSideCards } from '../Util.js';
 
-const sideCards = generateSideCards(6).map((val) => ({...val, flipped: true}));
+const sideCards = generateSideCards(6).map((val) => ({...val, flipped: false}));
 
 const aces = [
     {
@@ -36,6 +36,60 @@ const aces = [
 ];
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            sideCards: sideCards,
+            aces: aces,
+            lastFlippedPosition: -1
+        };
+
+        this.flipCard = this.flipCard.bind(this);
+        this.moveAce = this.moveAce.bind(this);
+    }
+
+    componentDidMount() {
+        setTimeout(this.flipCard, 1000);
+        // this.flipCard();
+    }
+
+    flipCard() {
+        let state = this.state;
+        let sideCards = state.sideCards;
+        let toFlipPosition = state.lastFlippedPosition + 1;
+        let cardToFlip = sideCards[toFlipPosition];
+        let flippedCard = {...cardToFlip, flipped: true};
+
+        sideCards[toFlipPosition] = flippedCard;
+
+        let newState = {
+            sideCards: sideCards,
+            lastFlippedPosition: toFlipPosition
+         };
+
+        this.setState({...state, ...newState});
+
+        setTimeout(this.moveAce, 1000);
+    }
+
+    moveAce() {
+        let lastFlippedCard = this.state.sideCards[this.state.lastFlippedPosition];
+        let cardSuit = lastFlippedCard.suit;
+
+        // find ace to move
+        let aces = this.state.aces;
+        let aceToMovePos = aces.findIndex((card) => card.suit == cardSuit);
+        let aceToMove = aces[aceToMovePos];
+        aceToMove = {...aceToMove, position: aceToMove.position + 1};
+
+        aces[aceToMovePos] = aceToMove;
+
+        this.setState(...this.state, aces: aces);
+
+        setTimeout(this.flipCard, 1000);
+    }
+
     render() {
         return (
             <View style={styles.container}>
@@ -44,11 +98,11 @@ export default class App extends Component {
                     <View style={styles.cardContainer}>
                         <SideCards
                             containerStyle={styles.markers}
-                            cards={sideCards} />
+                            cards={this.state.sideCards} />
 
                         <HorseCards
                             containerStyle={styles.aces}
-                            cards={aces} />
+                            cards={this.state.aces} />
                     </View>
                 </ScrollView>
             </View>
