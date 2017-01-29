0033e309a30ff06e611aed34f3b05d992a37be80
Josh Kasuboski <kasuboski@wisc.edu>
2017-01-29 15:37:23 -0600

    changed app name added scene style doesn't work

--- components/App.js
+++ components/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import {
+  View
+} from 'react-native';
 
 import { Router, Scene } from 'react-native-router-flux';
 
 import HomeScreen from './HomeScreen';
 import RaceScreen from './RaceScreen';
 
+const getSceneStyle = (/* NavigationSceneRendererProps */ props, computedProps) => {
+  const style = {
+    backgroundColor: '#8E443D',
+  };
+  return style;
+};
+
 const App = () => {
     return (
-        <Router>
+        <Router getSceneStyle={getSceneStyle}>
             <Scene key='root' hideNavBar={true}>
                 <Scene key='home'
                     component={HomeScreen}
